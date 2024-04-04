164bc9f4ec36afdf2e288d5ba1c6bb6eaca83f67
TomasUTN <Tomi.miranda272@gmail.com>
2024-04-03 21:05:19 -0300

    Grupo_6_11

---
+++ Funcionalidad_6_11.py
@@ -0,0 +1,26 @@
+# FUNCION 6
+
+def Calcular_promedio(suma,cantidad):
+        
+    """
+    Calcula el promedio entre la suma de las edades
+    :param suma: Resultado de la suma de todas las edades
+    :param cantidad: Cantidad de edades ingresadas
+    :return: Promedio de las edades ingresadas
+        
+    """
+    return suma/cantidad
+
+# FUNCION 11
+
+def orden_asc(edades):
+    """
+    Ordena listas de menor a mayor
+    :param edades: Lista con numeros
+    edades
+    :return: Lista con numeros ordenados de forma ascendente
+    """
+    lista_ordenada = sorted(edades)
+    return lista_ordenada
+
+
